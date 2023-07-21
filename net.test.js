84ff34483a6f1b6a13258fb574172668cfa06a6f
QA-Lexx <ilin_001@mail.ru>
2023-07-21 19:26:36 +0300

    create three tests

--- net.test.js
+++ net.test.js
@@ -12,44 +12,76 @@ afterEach(() => {
   page.close();
 });
 
-describe("Netology.ru tests", () => {
+describe("qamid.tmweb.ru tests", () => {
   beforeEach(async () => {
     page = await browser.newPage();
-    await page.goto("https://netology.ru");
+    await page.goto("http://qamid.tmweb.ru/client/index.php");
   });
 
-  test("The first test'", async () => {
-    const title = await page.title();
-    console.log("Page title: " + title);
-    await clickElement(page, "header a + a");
-    const title2 = await page.title();
-    console.log("Page title: " + title2);
-    const pageList = await browser.newPage();
-    await pageList.goto("https://netology.ru/navigation");
-    await pageList.waitForSelector("h1");
+  test("The first test one ticket'", async () => {
+    const linkNavigation = await pageNavigation.$("body > nav > a.page-nav__day.page-nav__day_weekend.page-nav__day_chosen");
+    await linkNavigation.click();
+    await page.waitForSelector("a");
+    const linkMovie = await pageMovie.$("body > main > section:nth-child(3) > div:nth-child(2) > ul > li > a");
+    await linkMovie.click();
+    await page.waitForSelector("a");
+    page = await browser.newPage();
+    await page.goto("http://qamid.tmweb.ru/client/hall.php");
+    const linkChair = await linkChair.$("body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(10) > span:nth-child(5)");
+    await linkChair.click();
+    await page.waitForSelector("span");
+    const linkBook = await linkBook.$("body > main > section > button");
+    await linkBook.click();
+    await page.waitForSelector("button");
+    page = await browser.newPage();
+    await page.goto("http://qamid.tmweb.ru/client/payment.php");
+    const titleBook = await page.title();
+    expect(titleBook).toEqual("Вы выбрали билеты:");
   });
 
-  test("The first link text 'Медиа Нетологии'", async () => {
-    const actual = await getText(page, "header a + a");
-    expect(actual).toContain("Медиа Нетологии");
+  test("The second test two tickets'", async () => {
+    const linkNavigation = await pageNavigation.$("body > nav > a.page-nav__day.page-nav__day_weekend.page-nav__day_chosen");
+    await linkNavigation.click();
+    await page.waitForSelector("a");
+    const linkMovie = await pageMovie.$("body > main > section:nth-child(3) > div:nth-child(2) > ul > li > a");
+    await linkMovie.click();
+    await page.waitForSelector("a");
+    page = await browser.newPage();
+    await page.goto("http://qamid.tmweb.ru/client/hall.php");
+    const linkChairOne = await linkChairOne.$("body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(10) > span:nth-child(5)");
+    await linkChairOne.click();
+    await page.waitForSelector("span");
+    const linkChairTwo = await linkChairTwo.$("body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(10) > span:nth-child(6)");
+    await linkChairTwo.click();
+    await page.waitForSelector("span");
+    const linkBook = await linkBook.$("body > main > section > button");
+    await linkBook.click();
+    await page.waitForSelector("button");
+    page = await browser.newPage();
+    await page.goto("http://qamid.tmweb.ru/client/payment.php");
+    const titleBook = await page.title();
+    expect(titleBook).toEqual("Вы выбрали билеты:");
   });
 
-  test("The first link leads on 'Медиа' page", async () => {
-    await clickElement(page, "header a + a");
-    const actual = await getText(page, ".logo__media");
-    await expect(actual).toContain("Медиа");
+  test("The third test no tickets'", async () => {
+    const linkNavigation = await pageNavigation.$("body > nav > a.page-nav__day.page-nav__day_weekend.page-nav__day_chosen");
+    await linkNavigation.click();
+    await page.waitForSelector("a");
+    const linkMovie = await pageMovie.$("body > main > section:nth-child(3) > div:nth-child(2) > ul > li > a");
+    await linkMovie.click();
+    await page.waitForSelector("a");
+    page = await browser.newPage();
+    await page.goto("http://qamid.tmweb.ru/client/hall.php");
+    const linkChair = await linkChair.$("body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(10) > span:nth-child(5)");
+    await linkChair.click();
+    await page.waitForSelector("span");
+    const linkBook = await linkBook.$("body > main > section > button");
+    await linkBook.click();
+    await page.waitForSelector("button");
+    page = await browser.newPage();
+    await page.goto("http://qamid.tmweb.ru/client/payment.php");
+    const titleBook = await page.title();
+    expect(titleBook).toEqual("Данное место занято");
   });
-});
 
-test("Should look for a course", async () => {
-  await page.goto("https://netology.ru/navigation");
-  await putText(page, "input", "тестировщик");
-  const actual = await page.$eval("a[data-name]", (link) => link.textContent);
-  const expected = "Тестировщик ПО";
-  expect(actual).toContain(expected);
-});
-
-test("Should show warning if login is not email", async () => {
-  await page.goto("https://netology.ru/?modal=sign_in");
-  await putText(page, 'input[type="email"]', generateName(5));
-});
+})
