1a7bf4d88b75b9a08a2cbc55ebefd5370b741d03
QA-Lexx <ilin_001@mail.ru>
2023-08-02 12:31:51 +0300

    add right changes in files with codes

--- features/search.feature
+++ features/search.feature
@@ -1,24 +1,16 @@
-# Feature: Search a course
-#     Scenario: Should search by text
-#         Given user is on "/navigation" page
-#         When user search by "тестировщик"
-#         Then user sees the course suggested "Тестировщик ПО"
+Feature: qamid.tmweb.ru tests
 
-Feature: qamid.tmweb.ru tests one test
     Scenario: The first test one ticket
-        Given user is on "/client/index.php" page
-        When user search by "user search by"
-        Then user sees the course suggested "Получить код бронирования"
+        Given first user is on "/client/index.php" page
+        When first user search by "user search by"
+        Then first user sees the course suggested "Получить код бронирования"
 
-Feature: qamid.tmweb.ru tests second test
     Scenario: The second test two tickets
-        Given user is on "/client/index.php" page
-        When user search by "user search by"
-        Then user sees the course suggested "Получить код бронирования"
+        Given second user is on "/client/index.php" page
+        When second user search by "user search by"
+        Then second user sees the course suggested "Получить код бронирования"
 
-Feature: qamid.tmweb.ru tests third test
     Scenario: The third test no tickets
-        Given user is on "/client/index.php" page
-        When user search by "user search by"
-        Then user sees the course suggested "Забронировать"
-        
+        Given third user is on "/client/index.php" page
+        When third user search by "user search by"
+        Then third user sees the course suggested "Забронировать"
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -4,35 +4,6 @@ const expect = chai.expect;
 const { Given, When, Then, Before, After } = require("cucumber");
 const { clickElement, putText, getText } = require("../../lib/commands.js");
 
-// Before({timeout: 60 * 1000}, async function () {
-//   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
-//   const page = await browser.newPage();
-//   this.browser = browser;
-//   this.page = page;
-// });
-
-// After({timeout: 60 * 1000}, async function () {
-//   if (this.browser) {
-//     await this.browser.close();
-//   }
-// });
-
-// Given({timeout: 60 * 1000}, "user is on {string} page", async function (string) {
-//   return await this.page.goto(`https://netology.ru${string}`, {
-//     setTimeout: 20000,
-//   });
-// });
-
-// When({timeout: 60 * 1000}, "user search by {string}", async function (string) {
-//   return await putText(this.page, "input", string);
-// });
-
-// Then({timeout: 60 * 1000}, "user sees the course suggested {string}", async function (string) {
-//   const actual = await getText(this.page, "a[data-name]");
-//   const expected = await string;
-//   expect(actual).contains(expected);
-// });
-
 // Cucumber tests
 
 Before({timeout: 60 * 1000}, async function () {
@@ -50,11 +21,11 @@ After({timeout: 60 * 1000}, async function () {
 
 // The first test one ticket
 
-Given("user is on {string} page", {timeout: 60 * 1000}, async function (string) {
+Given("first user is on {string} page", {timeout: 60 * 1000}, async function (string) {
   await this.page.goto(`http://qamid.tmweb.ru${string}`, {setTimeout: 10000});
 });
 
-When("user search by {string}", {timeout: 60 * 1000}, async function (string) {
+When("first user search by {string}", {timeout: 60 * 1000}, async function (string) {
   await clickElement(this.page, "body > nav > a:nth-child(3)", string);
   await clickElement(this.page, "body > main > section:nth-child(3) > div:nth-child(2) > ul > li > a", string);
   await this.page.goto(`http://qamid.tmweb.ru/client/hall.php`);
@@ -63,7 +34,7 @@ When("user search by {string}", {timeout: 60 * 1000}, async function (string) {
   await this.page.goto(`http://qamid.tmweb.ru/client/payment.php`);
 });
 
-Then("user sees the course suggested {string}", {timeout: 60 * 1000}, async function (string) {
+Then("first user sees the course suggested {string}", {timeout: 60 * 1000}, async function (string) {
   const actual = await getText(this.page, "body > main > section > div > button");
   const expected = await string;
   expect(actual).contains(expected);
@@ -73,11 +44,11 @@ Then("user sees the course suggested {string}", {timeout: 60 * 1000}, async func
 
 // The second test two tickets
 
-Given("user is on {string} page", {timeout: 60 * 1000}, async function (string) {
+Given("second user is on {string} page", {timeout: 60 * 1000}, async function (string) {
   await this.page.goto(`http://qamid.tmweb.ru${string}`, {setTimeout: 10000});
 });
 
-When("user search by {string}", {timeout: 60 * 1000}, async function (string) {
+When("second user search by {string}", {timeout: 60 * 1000}, async function (string) {
   await clickElement(this.page, "body > nav > a:nth-child(3)", string);
   await clickElement(this.page, "body > main > section:nth-child(3) > div:nth-child(2) > ul > li > a", string);
   await this.page.goto(`http://qamid.tmweb.ru/client/hall.php`);
@@ -87,7 +58,7 @@ When("user search by {string}", {timeout: 60 * 1000}, async function (string) {
   await this.page.goto(`http://qamid.tmweb.ru/client/payment.php`);
 });
 
-Then("user sees the course suggested {string}", {timeout: 60 * 1000}, async function (string) {
+Then("second user sees the course suggested {string}", {timeout: 60 * 1000}, async function (string) {
   const actual = await getText(this.page, "body > main > section > div > button");
   const expected = await string;
   expect(actual).contains(expected);
@@ -97,18 +68,18 @@ Then("user sees the course suggested {string}", {timeout: 60 * 1000}, async func
 
 // The third test no tickets
 
-Given("user is on {string} page", {timeout: 60 * 1000}, async function (string) {
+Given("third user is on {string} page", {timeout: 60 * 1000}, async function (string) {
   await this.page.goto(`http://qamid.tmweb.ru${string}`, {setTimeout: 10000});
 });
 
-When("user search by {string}", {timeout: 60 * 1000}, async function (string) {
+When("third user search by {string}", {timeout: 60 * 1000}, async function (string) {
   await clickElement(this.page, "body > nav > a:nth-child(3)", string);
   await clickElement(this.page, "body > main > section:nth-child(3) > div:nth-child(2) > ul > li > a", string);
   await this.page.goto(`http://qamid.tmweb.ru/client/hall.php`);
   await clickElement(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(10) > span:nth-child(5)", string);
 });
 
-Then("user sees the course suggested {string}", {timeout: 60 * 1000}, async function (string) {
+Then("third user sees the course suggested {string}", {timeout: 60 * 1000}, async function (string) {
   const actual = await getText(this.page, "body > main > section > button");
   const expected = await string;
   expect(actual).contains(expected);
